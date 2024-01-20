f46d6b8c99ce4d2a6cfbedf47e066e34b749254d
inder782 <inderchandel782@gmail.com>
2024-01-20 14:24:30 +0530

    improvements

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Section_4 from "@/components/section_4/Section_4";
 import Section_5 from "@/components/section_5/Section_5";
 import Section_6 from "@/components/section_6/Section_6";
 import Section_7 from "@/components/section_7/Section_7";
-import Section_8 from "@/components/section_8/Section_8";
+
 import React from "react";
 
 const page = () => {
@@ -20,7 +20,6 @@ const page = () => {
       <Section_5 />
       <Section_6 />
       <Section_7 />
-      <Section_8 />
     </div>
   );
 };
