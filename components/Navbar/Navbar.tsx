e91667a9d2ed2ac18710594fadb6daa8ac119b1a
inder782 <inderchandel782@gmail.com>
2024-02-12 16:12:55 +0530

    feat(navbar):responsive and a sheet for small devices (v1)

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,28 +1,50 @@
-import Link from "next/link";
 import React from "react";
 import "./styles.css";
+import Link from "next/link";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link href="/" className="title">
-        flowt
-      </Link>
-      <Link href="/" className="links">
-        Home
-      </Link>
-      <Link href="/" className="links">
-        Solutions
-      </Link>
-      <Link href="/" className="links">
-        Pricing
-      </Link>
-      <Link href="/" className="links">
-        Developers
-      </Link>
-      <div className="button">
-        <button className="login_btn">Log In</button>
-        <button className="publish_btn">Publish App</button>
+      <div className="logo">
+        <Link href="/">flowt</Link>
       </div>
+      <div className="links">
+        <Link href="/" className="p-8">
+          Home
+        </Link>
+        <Link href="/" className="p-8">
+          Solutions
+        </Link>
+        <Link href="/" className="p-8">
+          Pricing
+        </Link>
+        <Link href="/" className="p-8">
+          Developers
+        </Link>
+      </div>
+      <div className="buttons">
+        <button className="log_in">Log In</button>
+        <button className="btn_1">Publish App</button>
+      </div>
+      <Sheet>
+        <SheetTrigger className="min-[1435px]:hidden text-2xl">☰</SheetTrigger>
+        <SheetContent className="sheet">
+          {" "}
+          <Link href="/" className="p-8 ">
+            Home
+          </Link>
+          <Link href="/" className="p-8">
+            Solutions
+          </Link>
+          <Link href="/" className="p-8">
+            Pricing
+          </Link>
+          <Link href="/" className="p-8">
+            Developers
+          </Link>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
